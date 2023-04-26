f94eb44e1499d2408463f7b11d80b985e1fec162
iqbalarief <iqbal98arief@gmail.com>
2023-04-26 15:17:18 +0700

    "Initialize Project"

---
+++ src/test/java/com/iaspringframework/Learning/Spring/Boot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iaspringframework.Learning.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
